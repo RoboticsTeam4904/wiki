5130c28089b66cf45fa0d9240e58f169391f2f62
AJ <aj@ajnadel.com>
2016-01-18 14:46:59 -0800

    add navx install files

---
+++ files/navx/navxinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd
+curl -o ~/wpilib/java/current/lib/navx.jar http://www.t4904.xyz/wiki/files/navx/navx.jar
+curl -o ~/wpilib/java/current/lib/navx_frc.jar http://www.t4904.xyz/wiki/files/navx/navx_frc.jar
+curl -o ~/wpilib/java/current/ant/build.properties http://www.t4904.xyz/wiki/files/navx/build.properties
